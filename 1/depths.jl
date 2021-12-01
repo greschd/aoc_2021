6908872a8f7cb3dd5a52421c12aee6039e5ec28b
Dominik Gresch <mail@greschd.ch>
2021-12-01 22:01:19 +0100

    Add solution 1.2

--- 1/depths.jl
+++ 1/depths.jl
@@ -4,5 +4,12 @@ function count_increasing(values)
     sum(Int32(second > first) for (first, second) in zip(values, values[2:length(values)]))
 end
 
+function get_window_sums(values, window_size = 3)
+    index_offset = window_size - 1
+    [sum(values[i:i+index_offset]) for i in (1:length(values)-index_offset)]
+end
+
 lines = readlines(open("input.txt"))
-println(count_increasing([parse(Int32, i) for i in lines]))
+values = [parse(Int32, i) for i in lines]
+println("increasing:", count_increasing(values))
+println("increasing windows:", count_increasing(get_window_sums(values)))
