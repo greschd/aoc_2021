b0a8859b070cf60820ced83915acaaaec6e99a95
Dominik Gresch <mail@greschd.ch>
2021-12-07 07:05:19 +0100

    Slight improvement on day 7 solution

--- 07/crab_dance.jl
+++ 07/crab_dance.jl
@@ -1,20 +1,18 @@
 #!/usr/bin/env julia
 
-using Statistics
 using DelimitedFiles
 
-function get_min_fuel(positions, cost_func)
+function get_min_fuel(positions, cost_func = x -> x)
     fuel_needed = typemax(Int)
     for target in range(minimum(positions), maximum(positions), step = 1)
-        fuel_needed_current = sum(cost_func.(abs.(positions .- target)))
-        fuel_needed = minimum([fuel_needed, fuel_needed_current])
+        fuel_needed = minimum([fuel_needed, sum(cost_func.(abs.(positions .- target)))])
     end
     fuel_needed
 end
 
 function run_puzzle(filename)
     positions = readdlm(filename, ',', Int)
-    println(get_min_fuel(positions, x -> x))
+    println(get_min_fuel(positions))
     println(get_min_fuel(positions, x -> fld(x^2 + x, 2)))
 end
 
